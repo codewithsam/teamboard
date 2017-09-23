c70674602426d10c9d877b295385c97f299b275a
samcodex <codewithsam@outlook.com>
2017-09-23 14:03:57 +0530

    aa

--- modules/hbs-helpers/helpers.js
+++ modules/hbs-helpers/helpers.js
@@ -2,14 +2,10 @@ exports.charAtIndex = function (str, index) {
     let s = str.trim();
     return s[index].toUpperCase();
 }
-// exports.currentBoardData = function(object,property){
-// 	try{
-// 		var o = JSON.parse(object);
-// 	    return o[property];
-// 	}catch(e){
-		
-// 	}
-// }
+exports.currentBoardData = function(object,property){
+	var o = JSON.parse(object);
+	return o[property];
+}
 exports.getUser = function(user){
     console.log(user);
 }
