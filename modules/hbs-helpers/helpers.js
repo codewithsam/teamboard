aa8af5cae084c33e04b266dc984c752ee47215e5
samcodex <codewithsam@outlook.com>
2017-09-23 13:42:53 +0530

    some wierd bug commented out

--- modules/hbs-helpers/helpers.js
+++ modules/hbs-helpers/helpers.js
@@ -2,14 +2,14 @@ exports.charAtIndex = function (str, index) {
     let s = str.trim();
     return s[index].toUpperCase();
 }
-exports.currentBoardData = function(object,property){
-	try{
-		var o = JSON.parse(object);
-	    return o[property];
-	}catch(e){
+// exports.currentBoardData = function(object,property){
+// 	try{
+// 		var o = JSON.parse(object);
+// 	    return o[property];
+// 	}catch(e){
 		
-	}
-}
+// 	}
+// }
 exports.getUser = function(user){
     console.log(user);
 }
